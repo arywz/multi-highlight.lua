5ed5e53a25be16a549eabc265fa7233064a6cecf
ary.wz <arywz@users.noreply.github.com>
2022-04-23 16:35:27 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# multi-highlight.lua
-multiple highlight
+# multi-highlight.nvim
+
+Highlight multiple word with different colors
+
+<img src="" alt="screenshot" width="800"/>
+
+## Installation
+
+### [Packer](https://github.com/wbthomason/packer.nvim) 
+
+```
+use 'arywz/multi-highlight.nvim'
+```
+### [Vim-Plug](https://github.com/junegunn/vim-plug)
+
+```
+Plug 'arywz/multi-highlight.nvim'
+```
+
+## Configuration
+
+### Use Default
+```
+require'shade'.setup()
+```
+
+### Custom colors and key-mapping
+```
+require'shade'.setup({
+    -- you can add any number of colors you like
+    groups = {'#AEEE00', '#aa2222', '#22aa22', '#2222aa', '#B08020', '#FFA020', '#22aa22', '#8888ff'},
+    keys = {
+        ['<Space>m'] = {cmd='<cmd>lua require("multi-highlight").toggle_highlight()<cr>'},
+        ['<Space>M'] = {cmd='<cmd>lua require("multi-highlight").clear()<cr>'},
+    }
+})
+```
+
+## License
+
+Copyright (c) Ary.Wzh. Distributed under the MIT license
+
---
+++ lua/multi-highlight.lua
@@ -0,0 +1,71 @@
+local vim = vim
+local M = {}
+
+local group_table = {}
+
+local default_conf = {
+    groups = {'#88aa22', '#aa2222', '#22aa22', '#2222aa', '#B08020', '#FFA020', '#22aa22', '#8888ff'},
+    keys = {
+        ['<Space>m'] = {cmd='<cmd>lua require("multi-highlight").toggle_highlight()<cr>', mode = 'n', opt={}},
+        ['<Space>M'] = {cmd='<cmd>lua require("multi-highlight").clear()<cr>', mode = 'n', opt={}}
+    }
+}
+
+local function create_highlight_groups(conf)
+    group_table = {}
+    for index, group in ipairs(conf.groups) do
+        local group_name = 'MultiHighlight_'..index
+        local cmd = 'highlight '.. group_name .. ' guibg=' .. group
+        table.insert(group_table, {group_name=group_name, word = nil, match_id=nil})
+        vim.cmd(cmd)
+    end
+end
+
+local function clear_highlight(item)
+    vim.fn.matchdelete(item.match_id)
+    item.word = nil
+    item.match_id = nil
+end
+
+function M.setup (conf)
+    if conf then
+        conf = vim.tbl_extend('force', default_conf, conf)
+    else
+        conf = default_conf
+    end
+    create_highlight_groups(conf)
+    for key, cmd_opt in pairs(conf.keys) do
+        vim.api.nvim_set_keymap(cmd_opt.mode, key, cmd_opt.cmd, cmd_opt.opt)
+    end
+end
+
+function M.toggle_highlight(target_word)
+    local current_word = target_word or vim.fn.expand('<cword>')
+    for _, item in ipairs(group_table) do
+        if item.word and item.word == current_word then
+            clear_highlight(item)
+            return
+        end
+    end
+
+    for _, item in ipairs(group_table) do
+        if item.word == nil then
+            item.match_id = vim.fn.matchadd(item.group_name, '\\<'..current_word..'\\>')
+            item.word = current_word
+            return
+        end
+    end
+    vim.notify('ran out colors, total '..#group_table .. ' configured', vim.log.levels.WARN)
+end
+
+function M.clear()
+    for _, item in ipairs(group_table) do
+        if item.match_id then
+            clear_highlight(item)
+        end
+    end
+end
+
+M.setup()
+
+return M
